7b5fea897f43fe2fa7f43ef660a03b7461c2120c
conflabermits <christopher.dunaj@gmail.com>
2024-01-28 14:46:20 -0500

    Incorporate embed FS for static file serving

--- web/overlay.go
+++ web/overlay.go
@@ -1,14 +1,39 @@
 package overlay
 
 import (
+	"embed"
 	"fmt"
 	"net/http"
 )
+
+//go:embed static
+var content embed.FS
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	htmlContent, err := content.ReadFile("static/index.html")
+	if err != nil {
+		http.Error(w, "Error reading embedded HTML file", http.StatusInternalServerError)
+		return
+	}
+
+	// Set the Content-Type header
+	w.Header().Set("Content-Type", "text/html")
+
+	// Write the HTML content to the response
+	_, err = w.Write(htmlContent)
+	if err != nil {
+		http.Error(w, "Error writing response", http.StatusInternalServerError)
+		return
+	}
+}
 
 func WebOverlay() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, web!")
 	})
+
+	http.HandleFunc("/index", handleIndex)
+
 	fmt.Printf("Starting server on port 8080\n")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
