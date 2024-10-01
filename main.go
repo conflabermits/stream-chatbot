2b6a520e9e4be27d6fa3df14e2298dc83e2c6362
conflabermits <christopher.dunaj@gmail.com>
2024-09-30 22:37:28 -0400

    Add notes

--- main.go
+++ main.go
@@ -7,6 +7,16 @@ import (
 	overlay "stream-chatbot/web"
 )
 
+/*
+1. Check token validity
+    * [Twitch dev documentation - How to validate a token](https://dev.twitch.tv/docs/authentication/validate-tokens/#how-to-validate-a-token)
+    * `curl -X GET 'https://id.twitch.tv/oauth2/validate' -H 'Authorization: OAuth <token>'`
+2. If token is invalid, start oauth process, return token to synchronized channel
+    * [Go By Example - Channel Synchonization](https://gobyexample.com/channel-synchronization)
+3. Start chatbot
+4. Start web overlay
+*/
+
 func main() {
 	println("Started program!")
 	fmt.Printf("GOPATH is set to: %s\n", common.GetEnvVar("GOPATH"))
